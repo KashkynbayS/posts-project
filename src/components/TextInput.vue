bdc0cfafce4a3ec1d8f71fa37a2b6a05ec08c3ac
Sultan <87053811230s@gmail.com>
2024-06-26 19:54:39 +0500

    feat: add TextInput component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineEmits } from 'vue'
+
+const { id, placeholder } = defineProps({
+   id: String,
+   placeholder: String,
+   modelValue: String,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = event => {
+   emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+   <div class="form-item">
+      <input :id="id" :value="modelValue" @input="updateValue($event)" :placeholder="placeholder" />
+   </div>
+</template>
+
+<style scoped lang="scss">
+.form-item {
+   width: 100%;
+}
+</style>
