3da930b65cec1be6c790fc05cc532f330afa758c
Sultan <87053811230s@gmail.com>
2024-06-27 02:58:03 +0500

    feat: add new posts mock for profile

---
+++ src/mocks/myPosts.ts
@@ -0,0 +1,76 @@
+import type { Post } from '@/types/post'
+
+import PostImg from '@/assets/images/postImg.webp'
+
+export const myPosts: Post[] = [
+   {
+      id: '1',
+      title: 'My post 1',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '2',
+      title: 'My post 2',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '3',
+      title: 'My post 3',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '4',
+      title: 'My post 4',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '5',
+      title: 'My post 5',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '6',
+      title: 'My post 6',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '7',
+      title: 'My post 7',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '8',
+      title: 'My post 8',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '9',
+      title: 'My post 9',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+   {
+      id: '10',
+      title: 'My post 10',
+      description: "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+      image: PostImg,
+      countdown: '8m ago',
+   },
+]
